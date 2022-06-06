01aa67a44e522a75b39c28a971154a71a6796a54
denizozen <68877984+denizozen@users.noreply.github.com>
2022-06-06 12:18:41 +0300

    Add files via upload

---
+++ AbstractFactory.java
@@ -0,0 +1,170 @@
+import java.util.ArrayList;
+
+//Top Abstract Product class : FileSystem
+abstract class FileSystem{
+    abstract public String displayName();
+}
+
+//First Abstract Product class : File
+abstract class File extends FileSystem{
+    protected String name;
+    public String displayName(){
+        return name;
+    }
+}
+
+/* Concrete Product classes initialize the name variable appropriately for each file system type,
+ to be used in their parent Abstract Product class' displayName methods */
+
+//Concrete Product classes of Abstract Product class File : LinuxFile, BSDFile, NTFile
+class LinuxFile extends File{
+    public LinuxFile(){
+        name = "Linux File";
+        System.out.println("Linux file is created...");
+    }
+}
+
+class BSDFile extends File{
+    public BSDFile(){
+        name = "BSD File";
+        System.out.println("BSD file is created...");
+    }
+}
+
+class NTFile extends File{
+    public NTFile(){
+        name = "NT File";
+        System.out.println("NT file is created...");
+    }
+}
+
+//Second Abstract Product class : Directory
+abstract class Directory extends FileSystem{
+    protected String name;
+    public String displayName(){
+        return name;
+    }
+}
+
+//Concrete Product classes of Abstract Product class Directory : LinuxDirectory, BSDirectory, NTDirectory
+class LinuxDirectory extends Directory{
+    public LinuxDirectory(){
+        name = "Linux Directory";
+        System.out.println("Linux Directory is created...");
+    }
+}
+
+class BSDDirectory extends Directory{
+    public BSDDirectory(){
+        name = "BSD Directory";
+        System.out.println("BSD Directory is created...");
+    }
+}
+
+class NTDirectory extends Directory{
+    public NTDirectory(){
+        name = "NT Directory";
+        System.out.println("NT Directory is created...");
+    }
+}
+//Abstract Factory class : SystemFactory
+/*Since every system must have only one file system,
+we must use Singleton Pattern to ensure we can control the instances getting created */
+abstract class SystemFactory{
+    abstract public File createFile();
+    abstract public Directory createDirectory();
+    protected static SystemFactory instance = null;
+
+    public static SystemFactory getInstance() {
+        if(instance != null) {
+            System.out.println("Cannot create this instance!");
+        }
+        return instance;
+    }
+}
+//Concrete Factory classes of Abstract Factory class SystemFactory : LinuxFactory, BSDFactory, NTFactory
+/*Each Concrete Factory class overrides its parent's getInstance method to create their own factory instances, if possible */
+class LinuxFactory extends SystemFactory{
+
+    private LinuxFactory(){ }
+
+    public static SystemFactory getInstance(){
+        if (instance == null){
+            instance = new LinuxFactory();
+        }
+        return instance;
+    }
+    public LinuxFile createFile() {
+        return new LinuxFile();
+    }
+    public LinuxDirectory createDirectory() {
+        return new LinuxDirectory();
+    }
+}
+
+class BSDFactory extends SystemFactory{
+
+    private BSDFactory(){ }
+
+    public static SystemFactory getInstance(){
+        if (instance == null){
+            instance = new BSDFactory();
+        }
+        return instance;
+    }
+
+    public BSDFile createFile() {
+        return new BSDFile();
+    }
+    public BSDDirectory createDirectory() {
+        return new BSDDirectory();
+    }
+}
+
+class NTFactory extends SystemFactory{
+    private NTFactory(){ }
+
+    public static SystemFactory getInstance(){
+        if (instance == null){
+            instance = new NTFactory();
+        }
+        return instance;
+    }
+
+    public NTFile createFile() {
+        return new NTFile();
+    }
+    public NTDirectory createDirectory() {
+        return new NTDirectory();
+    }
+}
+
+//The Client class : BuildSystem
+class BuildSystem{
+    private ArrayList<FileSystem> fileSystems;
+    public void createSystem(SystemFactory factory){
+        fileSystems = new ArrayList<>();
+        fileSystems.add(factory.createDirectory());
+        fileSystems.add(factory.createFile());
+
+    }
+
+    public void displayFileSystem(){
+        System.out.println("\tListing File System\n\t--------------");
+        fileSystems.forEach(f -> System.out.println("\t" + f.displayName()));
+    }
+}
+//Abstract Factory Method Design Pattern
+
+public class AbstractFactory {
+    public static void main(String[] args){
+        //SystemFactory LINUX = LinuxFactory.getInstance();
+        //SystemFactory NT = NTFactory.getInstance();
+        SystemFactory BSD = BSDFactory.getInstance();
+
+        BuildSystem file = new BuildSystem();
+        System.out.println("Creating BSD");
+        file.createSystem(BSD);
+        file.displayFileSystem();
+    }
+}
